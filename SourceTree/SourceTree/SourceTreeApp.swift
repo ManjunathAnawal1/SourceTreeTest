93e610662de00eb5e040f7c186f46a62852e555a
Manjunath Anawal <manjunath.anawal@furlenco.com>
2022-10-15 11:50:09 +0530

    Initial Commits

---
+++ SourceTree/SourceTree/SourceTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceTreeApp.swift
+//  SourceTree
+//
+//  Created by Manjunath Anawal on 15/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
